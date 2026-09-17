7656e6cf10cd4c0f6bb4537bcdd7b10b3958f526
Hemkumar Janghel <124525311+hemkumar25@users.noreply.github.com>
2026-09-17 13:19:17 +0530

    ✅ 3Sum Closest | Medium | Java | Runtime: 18 ms (38.59%) | Memory: 45.5 MB | 2026-09-17

---
+++ leetcode/medium/0016-3sum-closest/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        
+
+        Arrays.sort(nums);
+
+        int ans = 0;
+        int closeness =Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i]== nums[i-1]){
+                continue;
+            }
+
+            int left = i+1;
+            int right = nums.length-1;
+
+            while(left<right){
+                int  sum = nums[i]+ nums[left] + nums[right];
+
+                int tempCloseness = Math.abs(sum - target);
+
+                if(tempCloseness < closeness){
+                    closeness= tempCloseness;
+                    ans = sum;
+                }
+                else if(sum<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
