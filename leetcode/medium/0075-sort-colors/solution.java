a5ebb5546104476b28ecc0b98f041f8e380ff132
Hemkumar Janghel <124525311+hemkumar25@users.noreply.github.com>
2026-09-19 13:12:25 +0530

    ✅ Sort Colors | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 43.7 MB | 2026-09-19

--- leetcode/medium/0075-sort-colors/solution.java
+++ leetcode/medium/0075-sort-colors/solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int low= 0 ; int mid = 0; int high=n-1;
+        int low= 0 ; int mid = 0; int high=nums.length-1;
 
         while(mid<=high){
             if(nums[mid]==0){
